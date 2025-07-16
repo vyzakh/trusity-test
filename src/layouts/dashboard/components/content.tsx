119930c58fac25798216faf09b06e3d010f59699
Sreekanth-Zoftcares <sreekanth.p@zoft.care>
2025-07-16 16:49:08 +0530

    fix

--- src/layouts/dashboard/components/content.tsx
+++ src/layouts/dashboard/components/content.tsx
@@ -7,7 +7,7 @@ export default function Content() {
   return (
     <main className="font-helvetica flex grow flex-col sm:ml-[var(--sidebar-width)]">
       <div
-        className="container mx-auto flex max-w-screen-2xl grow flex-col px-4 py-4 sm:py-6 lg:py-8 xl:px-6 2xl:px-8"
+        className="container flex max-w-screen-2xl grow flex-col py-4"
         id="dashboard-layout"
       >
         <React.Suspense fallback={<RenderFallback />}>
